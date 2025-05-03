a7ba2371128c5bb6806f5e39b3f8a2e9585e8b59
Alexandre Negrel <alexandre@negrel.dev>
2025-05-03 22:36:27 +0200

    add hot reload support

--- mkwebsite.sh
+++ mkwebsite.sh
@@ -12,9 +12,9 @@ eval "$(log4bash)"
 : "${CACHE_DIR:="cache"}"
 : "${HOST:="127.0.0.1"}"
 : "${PORT:=8080}"
+: "${MKWEBSITE_ENV:="dev"}"
 
 # Globals
-export MKWEBSITE_ENV="dev"
 declare -a \
 	pre_setup_phase setup_phase post_setup_phase \
 	pre_build_phase build_phase post_build_phase \
@@ -110,6 +110,8 @@ watch() {
 	load_modules
 	build
 
+	cp "$0" "$OUT_DIR/reload.sh"
+
 	# Start light http server in background.
 	lighttpd -D -f <(cat <<EOF
 server.document-root = "$(realpath "$OUT_DIR")"
@@ -117,17 +119,41 @@ server.bind = "$HOST"
 server.port = $PORT
 
 server.modules = (
-  "mod_accesslog",
-  "mod_indexfile"
+	"mod_accesslog",
+	"mod_indexfile",
+	"mod_setenv",
+	"mod_cgi"
 )
 
 accesslog.filename = "$(tty)"
 server.errorlog = "$(tty)"
+server.breakagelog = "$(tty)"
 
 # File to search for when a directory is requested (e.g. /)
 index-file.names = ("index.html", "index.htm")
+
+# Allow websocket upgrade.
+cgi.upgrade = "enable"
+# Hot reload CGI script.
+cgi.assign = ( "/reload.sh" => "" )
+# Forward PATH for CGI scripts.
+setenv.set-environment = (
+	"PATH" => "$PATH",
+	"OUT_DIR" => "$(realpath "$OUT_DIR")",
+	"MKWEBSITE_ENV" => "cgi"
+)
 EOF
 ) &
+
+cat <<EOF > "$OUT_DIR/reload.js"
+(() => {
+  const socket = new WebSocket('/reload.sh');
+  socket.addEventListener('error', console.error);
+  socket.addEventListener('close', () => {
+    window.location.reload()
+  });
+})();
+EOF
 
 	# Watch for file changes.
 	inotifywait \
@@ -143,10 +169,41 @@ EOF
 				done
 				# Then build once.
 				build
+
+				# Close reload websockets.
+				xargs kill < "$OUT_DIR/reload.pid" || true
+				rm "$OUT_DIR/reload.pid"
 			done
 }
 
+# CGI hot reload websocket handler.
+cgi_reload_wss() {
+	# Store PID.
+	echo $$ >> "$OUT_DIR/reload.pid"
+
+	# See https://www.rfc-editor.org/rfc/rfc6455.html
+	local guid="258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+	local key
+	key="$(echo -n "$HTTP_SEC_WEBSOCKET_KEY$guid" | sha1sum | cut -d ' ' -f 1 | xxd -p -r | base64)"
+
+	log_info "hot reload websocket initialization"
+	# Upgrade to web socket.
+	echo "Status: 101"
+	echo "Upgrade: WebSocket"
+	echo "Connection: Upgrade"
+	echo "Sec-WebSocket-Accept: $key"
+	echo ""
+	# Sleep forever to keep socket open.
+	while true; do sleep 1; done
+}
+
 main() {
+	# Hot reload CGI handler.
+	if [ "$MKWEBSITE_ENV" = "cgi" ]; then
+		cgi_reload_wss
+		exit 0
+	fi
+
 	if [ "$#" = "0" ]; then
 		print_help
 		exit 1
